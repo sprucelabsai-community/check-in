bc8505c8a5bea55ea0a93e4ab6aa65c9666dee1e
Taylor Romero <me@taylorrome.ro>
2023-11-05 07:25:25 -0800

    patch: upgrade

--- src/.spruce/events/appointments/listPeopleCapabilities.v2021_06_23.contract.ts
+++ src/.spruce/events/appointments/listPeopleCapabilities.v2021_06_23.contract.ts
@@ -1,30 +1,18 @@
 import '#spruce/permissions/permissions.types'
 import { buildEventContract } from '@sprucelabs/mercury-types'
-import { buildPermissionContract } from '@sprucelabs/mercury-types'
 import listPeopleCapabilitiesEmitTargetAndPayloadSchema from '#spruce/schemas/appointments/v2021_06_23/listPeopleCapabilitiesEmitTargetAndPayload.schema'
 import listPeopleCapabilitiesResponsePayloadSchema from '#spruce/schemas/appointments/v2021_06_23/listPeopleCapabilitiesResponsePayload.schema'
 
 const listPeopleCapabilitiesEventContract = buildEventContract({
 	eventSignatures: {
 		'appointments.list-people-capabilities::v2021_06_23': {
+			emitPermissions: {
+				contractId: 'appointments.management',
+				permissionIdsAny: ['can-configure-people-capabilities'],
+			},
+
 			emitPayloadSchema: listPeopleCapabilitiesEmitTargetAndPayloadSchema,
 			responsePayloadSchema: listPeopleCapabilitiesResponsePayloadSchema,
-			emitPermissionContract: buildPermissionContract({
-				id: 'appointments.listPeopleCapabilitiesEmitPermissions',
-				name: 'list people capabilities',
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'can-list-people-capabilities',
-						name: 'Can list people capabilities',
-						defaults: {
-							owner: {
-								default: true,
-							},
-						},
-					},
-				],
-			}),
 		},
 	},
 })
--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -5,38 +5,35 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'checkin.checkin': [
 			'can-get-guest-details','can-checkin-self',
 		]
+		'heartwood.skill-views': [
+			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
+		]
 		'appointments.appointments': [
 			'can-hold-time','can-get-monthly-aviability','can-cancel-hold','can-book-appointment','can-create-appointment','can-list-appointments','can-list-appointments-for-others','can-update-appointment','can-listen-to-did-book',
 		]
 		'appointments.management': [
 			'can-configure-people-capabilities','can-configure-role-capabilities','can-list-role-capabilities','can-register-capabilities','can-get-organization-settings',
 		]
-		'appointments.createCategoryEmitPermissions': [
-			'can-create-category',
-		]
-		'appointments.deleteCategoryEmitPermissions': [
-			'can-delete-category',
-		]
-		'appointments.getEmitPermissions': [
-			'can-get-appointment',
-		]
-		'appointments.deleteServiceEmitPermissions': [
-			'can-delete-service',
-		]
-		'appointments.getCategoryEmitPermissions': [
-			'can-get-category',
-		]
-		'appointments.getServiceEmitPermissions': [
-			'can-get-service',
+		'appointments.listBookedServicesEmitPermissions': [
+			'can-list-booked-services',
 		]
 		'appointments.getAvailableTimesEmitPermissions': [
 			'can-get-available-times',
 		]
-		'appointments.listCategoriesEmitPermissions': [
-			'can-list-categories',
+		'appointments.createServiceEmitPermissions': [
+			'can-create-service',
 		]
-		'appointments.listBookedServicesEmitPermissions': [
-			'can-list-booked-services',
+		'appointments.deleteCategoryEmitPermissions': [
+			'can-delete-category',
+		]
+		'appointments.deleteServiceEmitPermissions': [
+			'can-delete-service',
+		]
+		'appointments.getEmitPermissions': [
+			'can-get-appointment',
+		]
+		'appointments.createCategoryEmitPermissions': [
+			'can-create-category',
 		]
 		'appointments.willSendConfirmationListenPermissions': [
 			'can-listen-to-will-send',
@@ -44,32 +41,32 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'appointments.listServicesEmitPermissions': [
 			'can-list-services',
 		]
-		'appointments.syncStatusesEmitPermissions': [
-			'can-high-five',
+		'appointments.listCategoriesEmitPermissions': [
+			'can-list-categories',
 		]
 		'appointments.listStatusesEmitPermissions': [
 			'can-list-statuses',
 		]
-		'appointments.updateCategoryEmitPermissions': [
-			'can-edit-category',
-		]
-		'appointments.listPeopleCapabilitiesEmitPermissions': [
-			'can-list-people-capabilities',
-		]
-		'appointments.updateOrganizationSettingsEmitPermissions': [
-			'can-save-org-settings',
-		]
 		'appointments.updateServiceEmitPermissions': [
 			'can-edit-service',
 		]
-		'appointments.createServiceEmitPermissions': [
-			'can-create-service',
+		'appointments.syncStatusesEmitPermissions': [
+			'can-high-five',
 		]
 		'appointments.listServicesWithProvidersEmitPermissions': [
 			'can-list-services-with-providers',
 		]
-		'heartwood.skill-views': [
-			'can-get-skill-views','can-register-skill-views','can-listen-to-did-register-skill-views','can-generate-url','can-get-skill-views','can-list-skill-views','can-get-dashboard-cards','can-get-active-theme','can-manage-organization-themes',
+		'appointments.getCategoryEmitPermissions': [
+			'can-get-category',
+		]
+		'appointments.getServiceEmitPermissions': [
+			'can-get-service',
+		]
+		'appointments.updateOrganizationSettingsEmitPermissions': [
+			'can-save-org-settings',
+		]
+		'appointments.updateCategoryEmitPermissions': [
+			'can-edit-category',
 		]
 	}
 }
