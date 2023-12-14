d46903e62227a78fce8dfd8929b2535b71c426db
CircleCI <devops@sprucelabs.ai>
2023-12-14 20:37:52 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/listeners.ts
+++ src/.spruce/events/listeners.ts
@@ -2,13 +2,6 @@ import { EventFeatureListener } from '@sprucelabs/spruce-event-utils'
 
 const listeners: EventFeatureListener[] = [
     {
-        eventName: 'did-boot',
-        eventNamespace: 'skill',
-        version: 'v2023_05_07',
-        callback: require('../../listeners/skill/did-boot.v2023_05_07.listener').default,
-        isGlobal: require('../../listeners/skill/did-boot.v2023_05_07.listener').isGlobal,
-    },
-    {
         eventName: 'checkin',
         eventNamespace: 'checkin',
         version: 'v2023_05_07',
@@ -21,6 +14,13 @@ const listeners: EventFeatureListener[] = [
         version: 'v2023_05_07',
         callback: require('../../listeners/checkin/get-guest.v2023_05_07.listener').default,
         isGlobal: require('../../listeners/checkin/get-guest.v2023_05_07.listener').isGlobal,
+    },
+    {
+        eventName: 'did-boot',
+        eventNamespace: 'skill',
+        version: 'v2023_05_07',
+        callback: require('../../listeners/skill/did-boot.v2023_05_07.listener').default,
+        isGlobal: require('../../listeners/skill/did-boot.v2023_05_07.listener').isGlobal,
     },
     {
         eventName: 'did-book',
